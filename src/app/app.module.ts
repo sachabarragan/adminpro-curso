3186356561bcaa5048c101872fa5caa3d3163563
SachaBa <barragan.sacha@gmail.com>
2019-10-12 14:59:56 -0300

    Mi primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { APP_ROUTES } from './app.routes';
+
+// Routes
+
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Graphics1Component } from './pages/graphics1/graphics1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graphics1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumsComponent,
+    PagesComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    APP_ROUTES
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
