909ecc154f23357ffb46b2c5746ea473f22dbebc
SachaBa <barragan.sacha@gmail.com>
2019-10-23 20:37:58 -0300

    Agregamos y modificamos modulos

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graphics1Component } from './graphics1/graphics1.component';
+import { PagesComponent } from './pages.component';
+import { SharedModule } from '../shared/shared.module';
+import { PAGES_ROUTES } from './pages.routes';
+
+
+@NgModule ({
+    declarations: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graphics1Component
+    ],
+    exports: [
+        DashboardComponent,
+        ProgressComponent,
+        Graphics1Component
+    ],
+    imports: [
+        SharedModule,
+        PAGES_ROUTES
+    ]
+})
+
+// tslint:disable-next-line: eofline
+export class PagesModule {}
---
+++ src/app/pages/pages.routes.ts
@@ -0,0 +1,22 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graphics1Component } from './graphics1/graphics1.component';
+
+const pagesRoutes: Routes = [
+    {
+        path: '',
+        component: PagesComponent,
+        children: [
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'progress', component: ProgressComponent },
+            { path: 'graphics1', component: Graphics1Component },
+            { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+        ]
+    },
+];
+
+// tslint:disable-next-line: eofline
+export const PAGES_ROUTES = RouterModule.forChild( pagesRoutes );
